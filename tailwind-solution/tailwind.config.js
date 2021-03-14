27dce21f08e63ee2105a728b1430fb2b2a80008f
Stefan Turner <turner.stefan281@gmail.com>
2021-03-14 12:48:41 +0100

    add tailwind solution

---
+++ tailwind-solution/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      bg: '#e5eff5',
+      primary: '#2ab2af',
+      white: '#ffffff',
+      header2: '#c0df34',
+      text: '#98a6bd',
+    },
+    fontFamily: {
+      karla: ['Karla', 'sans-serif'],
+    }, 
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
